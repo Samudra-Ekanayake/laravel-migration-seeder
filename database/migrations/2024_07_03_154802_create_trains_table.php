0ab6af59eb748667e41e2fc4ace695033c646d41
Samudra-Ekanayake <samudra2724@gmail.com>
2024-07-04 17:10:59 +0200

    colonne fatte

--- database/migrations/2024_07_03_154802_create_trains_table.php
+++ database/migrations/2024_07_03_154802_create_trains_table.php
@@ -12,8 +12,29 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('trains', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
+            /*  $table->id();
+            $table->string('Azienda');
+            $table->string('Stazione di partenza');
+            $table->string('Stazione di arrivo');
+            $table->string('orario di partenza');
+            $table->timestamps('orario di arrivo');
+            $table->id('codice treno');
+            $table->timestamps('numero carrozze');
+            $table->id('in orario');
+            $table->timestamps('cancellato'); */
+
+            $table->bigIncrements('id');
+            $table->string('Azienda');
+            $table->string('Stazione di partenza');
+            $table->string('Stazione di arrivo');
+            $table->timestamp('orario di partenza')->nullable();
+            $table->timestamp('orario di arrivo')->nullable();
+            $table->bigInteger('codice treno')->unsigned();
+            $table->timestamp('created_at')->nullable();
+            $table->timestamp('updated_at')->nullable();
+            $table->bigInteger('numero carrozze')->unsigned();
+            $table->bigInteger('in orario')->unsigned();
+            $table->timestamp('cancellato')->nullable();
         });
     }
 
